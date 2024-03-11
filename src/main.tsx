55e557c73b1e44bba40d7066746cb73c9958fc2e
derickferreira <derickferreira036@gmail.com>
2024-03-11 11:18:12 -0300

    Default template

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// routes
+import Home from "./routes/Home.tsx";
+import Shop from "./routes/Shop.tsx";
+import About from "./routes/About.tsx";
+import Contact from "./routes/Contact.tsx";
+import ErrorPage from "./routes/ErrorPage.tsx";
+
+// react-router -- config
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/shop",
+        element: <Shop />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
