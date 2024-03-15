01bf1f3731bdcaedfd0cebb8af0382655f3e0f6f
derickferreira <derickferreira036@gmail.com>
2024-03-15 14:45:39 -0300

    Creating home

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,10 +1,29 @@
 import classes from "./style/Home.module.css";
 
+// hooks
+import { useFetch } from "../hooks/useFetch";
+
+const url = "http://localhost:3000/products";
+
 const Home = () => {
+  const { data } = useFetch(url);
+
   return (
     <div className={classes.home_container}>
-      <ul className="promotions_product">
-
+      <ul className={classes.promotions_product}>
+        {data?.slice(0, 3).map((item) => (
+          <li key={item.id} className={classes.product_container}>
+            <div className={classes.image_container}>
+              <img src={item.imageURL} alt={item.title} />
+            </div>
+            <h4>{item.title}</h4>
+            <div className={classes.price_container}>
+              <span className={classes.risk_price}>{item.riskPrice}</span>
+              <span>{item.price}</span>
+            </div>
+            <button>Add to cart</button>
+          </li>
+        ))}
       </ul>
     </div>
   );
